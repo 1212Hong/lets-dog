f5ca87d5149702f0d5e745dd8eea43370f1cb456
Young Seob So <9412sys@gmail.com>
2023-10-26 09:45:32 +0900

    [FE]feat: 레트로핏 디펜더시 추가

--- FE/dog/app/build.gradle.kts
+++ FE/dog/app/build.gradle.kts
@@ -61,6 +61,8 @@ buildscript {
 
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.navigation:navigation-compose:2.7.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
