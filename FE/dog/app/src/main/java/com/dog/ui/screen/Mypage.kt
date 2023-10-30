ac9ac5aecee8bec230beaf7782c00c69fa1e1b18
권영재 <dwdjjj@naver.com>
2023-10-30 15:45:15 +0900

    [FE] Feat : Mypage ui

--- FE/dog/app/src/main/java/com/dog/ui/screen/Mypage.kt
+++ FE/dog/app/src/main/java/com/dog/ui/screen/Mypage.kt
@@ -1,13 +1,16 @@
 package com.dog.ui.screen
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,11 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.dog.ui.components.MainButton
 import com.dog.ui.theme.DogTheme
+import com.dog.ui.theme.Purple400
 
 @Composable
 fun MypageScreen(navController: NavController) {
@@ -35,24 +38,69 @@ fun MypageScreen(navController: NavController) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
+                // 프로필 이미지
                 Box(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp)
-                        .padding(horizontal = 15.dp, vertical = 10.dp)
-                        .clip(MaterialTheme.shapes.large)
+                        .size(120.dp) // 이미지 크기 조절
+                        .clip(CircleShape) // 이미지를 원형으로 클리핑
+                        .background(Purple400) // 배경색
                 ) {
-                    Image(
-                        painter = painterResource(androidx.core.R.drawable.ic_call_answer),
-                        contentDescription = "profile_screen_bg",
-                        contentScale = ContentScale.Crop
+                    // 여기에 프로필 이미지를 로드하는 코드를 추가할 수 있습니다.
+                    // 예: CoilImage 또는 painterResource를 사용하여 이미지 로드
+                }
+
+                // 사용자 이름
+                Text(
+                    text = "소영섭",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
+
+                // 사용자 정보
+                Text(
+                    text = "새벽 산책 메이트 구해요",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+
+                // 팔로워 및 팔로잉 수
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "Followers: 1000",
+                        style = MaterialTheme.typography.bodySmall
+                    )
+
+                    Text(
+                        text = "Following: 500",
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                    Text(
+                        text = "#귀여움 #소형견",
+                        style = MaterialTheme.typography.bodySmall
                     )
                 }
-                Text(
-                    "Mypage Screen",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.padding(vertical = 20.dp)
-                )
+                Column {
+                    // 편집 버튼
+                    Button(
+                        onClick = {
+                            // navController.navigate("edit_profile")
+                        },
+                        modifier = Modifier
+                            .padding(vertical = 16.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Text(text = "프로필 편집")
+                    }
+                    Row() {
+                        MainButton(onClick = {}, text = "친구 신청")
+                        MainButton(onClick = {}, text = "차단")
+                    }
+
+                }
+
             }
         }
     }
